ea2168a30f2b8616cb05e393723e08ef916099eb
Takuto Minamii <takuto.minamii@nijibox.co.jp>
2026-03-06 19:07:05 +0900

    MS003-Daily_Dish-Project
    開発環境構築：デプロイエラー対応

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from 'next'
 const withPWA = require('next-pwa')({
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    skipWaiting: true,
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
 })
-
 const nextConfig: NextConfig = {
-    /* 既存の設定がある場合はここに追記 */
+  turbopack: {},
 }
-
 export default withPWA(nextConfig)
